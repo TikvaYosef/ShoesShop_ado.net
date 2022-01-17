34baefe86040e1e975ca40521d8f4ef8d159bd9d
TikvaYosef <tikvayosef13@gmail.com>
2022-01-17 20:37:28 +0200

    commit

--- Controllers/API/SportController.cs
+++ Controllers/API/SportController.cs
@@ -15,75 +15,88 @@ namespace ShoesShop_ado.net.Controllers.API
         List<SportShoe> shoe = new List<SportShoe>();
 
         // GET: api/Sport
-        public List<SportShoe> ShowAllSportShoes(List<SportShoe> shoe, string connectionstrin)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionstrin))
-            {
-                connection.Open();
-                string query = @"SELECT * FROM sportshoe ";
-                SqlCommand command = new SqlCommand(query, connection);
-                SqlDataReader datafromdb = command.ExecuteReader();
-                if (datafromdb.HasRows)
-                {
-                    while (datafromdb.Read())
-                    {
-                        shoe.Add(new SportShoe(datafromdb.GetString(1), datafromdb.GetInt32(2), datafromdb.GetInt32(3)));
-
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("no rows in table");
-                }
-                connection.Close();
-
-
-            }
-                return shoe;
-        }
 
         public IHttpActionResult Get()
         {
-            List<SportShoe> shoes = ShowAllSportShoes(shoe, connectionstrin);
-            return Ok(new { shoes });
-        }
-
-
-
-
-
-
-        // GET: api/Sport/5
-
-        public List<SportShoe> ShowShoe(string connectionstrin, List<SportShoe> shoe,int id)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionstrin))
+            try
             {
-                connection.Open();
-                string query = $@"SELECT * FROM sportshoe WHERE Id = {id}";
-                SqlCommand command = new SqlCommand(query, connection);
-                SqlDataReader datafromdb = command.ExecuteReader();
-                if (datafromdb.HasRows)
+                using (SqlConnection connection = new SqlConnection(connectionstrin))
                 {
-                    while (datafromdb.Read())
+                    connection.Open();
+                    string query = @"SELECT * FROM sportshoe ";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader datafromdb = command.ExecuteReader();
+                    if (datafromdb.HasRows)
                     {
-                        shoe.Add(new SportShoe(datafromdb.GetString(1), datafromdb.GetInt32(2), datafromdb.GetInt32(3)));
+                        while (datafromdb.Read())
+                        {
+                            shoe.Add(new SportShoe(datafromdb.GetString(1), datafromdb.GetInt32(2), datafromdb.GetInt32(3)));
+
+                        }
                     }
+                    else
+                    {
+                        Console.WriteLine("no rows in table");
+                    }
+                    connection.Close();
+
+                    return Ok(shoe);
                 }
-                else
-                {
-                     Console.WriteLine("no rows in table");
-                }
+
 
             }
-
-            return shoe;
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
+
+
+
+
+
+
+        //GET: api/Sport/5
 
         public IHttpActionResult Get(int id)
         {
-            List<SportShoe> shoes = ShowShoe(connectionstrin, shoe, id);
-            return Ok(new { shoes });
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionstrin))
+                {
+                    connection.Open();
+                    string query = $@"SELECT * FROM sportshoe WHERE Id = {id}";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader datafromdb = command.ExecuteReader();
+                    if (datafromdb.HasRows)
+                    {
+                        while (datafromdb.Read())
+                        {
+                            shoe.Add(new SportShoe(datafromdb.GetString(1), datafromdb.GetInt32(2), datafromdb.GetInt32(3)));
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("no rows in table");
+                    }
+                    return Ok(shoe);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
         }
 
 
@@ -101,7 +114,7 @@ namespace ShoesShop_ado.net.Controllers.API
                     SqlCommand command = new SqlCommand(query, connection);
                     int rowEffected = command.ExecuteNonQuery();
                     connection.Close();
-                    
+
                     return Ok(rowEffected);
                 }
             }
@@ -113,7 +126,7 @@ namespace ShoesShop_ado.net.Controllers.API
             {
                 return BadRequest(ex.Message);
             }
-           
+
         }
 
         // PUT: api/Sport/5
@@ -140,7 +153,7 @@ namespace ShoesShop_ado.net.Controllers.API
             {
                 return BadRequest(ex.Message);
             }
-            
+
         }
 
         // DELETE: api/Sport/5
